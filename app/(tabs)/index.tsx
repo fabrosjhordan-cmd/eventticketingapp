6a2fa6da0dd98ff55bdc0f874f56f64e9e258880
fabrosjhordan-cmd <fabrosjhordan@gmail.com>
2025-09-25 14:57:56 +0800

    Installation of Modules and Nativewind

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+
+import { Text, View } from '@/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-6xl font-semibold text-red'>Hello World!</Text>
+    </View>
+  );
+}
+
